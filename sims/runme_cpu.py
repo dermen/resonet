e18de172a28565b7a2dfe1b3b1f382cbefaa8fe0
Derek Mendez <dermen@lbl.gov>
2023-01-17 07:51:59 -0800

    multi latt updates

--- sims/runme_cpu.py
+++ sims/runme_cpu.py
@@ -15,8 +15,11 @@ from dials.array_family import flex
 # load a detector and beam model
 import make_sims
 import paths_and_const
+import sys
 
-np.random.seed(0)
+seed = int(sys.argv[1])
+
+np.random.seed(seed)
 
 
 class Simulator:
@@ -33,17 +36,15 @@ class Simulator:
         self.nb_beam = make_crystal.load_beam(self.BEAM,
                                       divergence=paths_and_const.DIVERGENCE_MRAD / 1e3 * 180 / np.pi)
 
-
-    def simulate(self, rot_mat=None, multi_lattice=False):
+    def simulate(self, rot_mat=None, multi_lattice_chance=0, ang_sigma=0.01, num_lat=2):
         """
 
         :param rot_mat: nominal rotation matrix
-        :param multi_lattice: a 2-tuple, where first element is the number of additional lattices to simulate
-            and the second element is the stdev. of a gaussian (in degrees) used to determine perturbation angles
+        :param multi_lattice_chance:
         :return:
         """
         pdb_name = make_sims.choose_pdb()
-        C = make_crystal.load_crystal(pdb_name, rot_mat, use_p1=True)
+        C = make_crystal.load_crystal(pdb_name, rot_mat, use_p1=False)
         mos_spread, mos_dom = make_sims.choose_mos()
         print("Simulating for %s" % pdb_name)
         print(mos_spread, mos_dom)
@@ -64,8 +65,8 @@ class Simulator:
         print("sim spots!")
         S.D.add_nanoBragg_spots_cuda()
         spots = S.D.raw_pixels.as_numpy_array()
-        if multi_lattice is not None:
-            num_lat, ang_sigma = multi_lattice
+        use_multi = np.random.random() < multi_lattice_chance
+        if use_multi:
             mats = Rotation.random(num_lat).as_matrix()
             vecs = np.dot(np.array([1, 0, 0])[None], mats)[0]
             angs = np.random.normal(0, ang_sigma, num_lat)
@@ -99,29 +100,40 @@ class Simulator:
         S.D.add_noise()
         noise_img = S.D.raw_pixels.as_numpy_array().reshape(img.shape)
 
-        return noise_img
+        param_dict = {"reso": reso,
+                      "multi_lattice": use_multi,
+                      "ang_sigma": ang_sigma,
+                      "num_lat": num_lat}
 
-
+        return param_dict, noise_img
 
 
 if __name__ == "__main__":
+    from sx.diffraction_ai import eval_model
+
     fnames = glob.glob("/mnt/data/s2/blstaff/SOLTIS/AI_PREDICTION/3.15A/*cbf")
     loader = dxtbx.load(fnames[0])
     D = loader.get_detector()
     D0 = utils.set_detector_thickness(D)
     BEAM = loader.get_beam()
-    mask = loader.get_raw_data().as_numpy_array() < 0
+    mask = loader.get_raw_data().as_numpy_array() >= 0
 
     HS = Simulator(D0, BEAM)
-    Nshot = 2
+    Nshot = 15
     multi_lattice = 2,.01
     multi_lattice = None
+    all_param = []
 
-    with h5py.File("holtonator_tester_rank%d_HiSym2.h5" % COMM.rank, "w") as out:
+    with h5py.File("tester_rank%d_multLats.h5" % COMM.rank, "w") as out:
         out.create_dataset("mask", data=mask)
         dset = out.create_dataset("images",
+                                  shape=(Nshot,) + (512,512),
+                                  dtype=np.float32)
+
+        raw_dset = out.create_dataset("raw_images",
                                   shape=(Nshot,) + tuple(mask.shape),
                                   dtype=np.float32)
+
 
         rotMats = Rotation.random(Nshot).as_matrix()
         times = []
@@ -129,10 +141,16 @@ if __name__ == "__main__":
             if i_shot % COMM.size != COMM.rank :
                 continue
             t = time.time()
-            img = HS.simulate(rot_mat=rotMats[i_shot], multi_lattice=multi_lattice)
-            dset[i_shot] = img
+            params, img = HS.simulate(rot_mat=rotMats[i_shot],
+                                      ang_sigma=0.1,
+                                      multi_lattice_chance=1)
+            all_param.append(
+                [params["reso"], params["multi_lattice"]])
+            quad = eval_model.raw_img_to_tens_pil(img, mask)
+            raw_dset[i_shot] = img
+            dset[i_shot] = quad
             t = time.time()-t
             times.append(t)
             print("Done with shot %d / %d (took %.4f sec)" % (i_shot+1, Nshot, t))
+        out.create_dataset("labels", data=all_param)
         print("Done! Takes %.4f sec on average per image" % np.mean(times))
-
