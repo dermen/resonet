f1a4e09269c57bad065c9a1aa879fb91635ac8f6
dermen <dermen@lbl.gov>
2023-02-01 20:55:56 -0800

    data loader for pngs

---
+++ loaders.py
@@ -0,0 +1,94 @@
+
+import os
+import re
+import glob
+import pandas
+from torch.utils.data import Dataset, DataLoader
+import torch
+import numpy as np
+
+from PIL import Image
+
+
+class TorchDset(Dataset):
+
+    def __init__(self, pngdir=None, propfile=None, quad="A", start=None, stop=None,
+                 dev=None, invert_res=True):
+        if pngdir is None:
+            pngdir = "/global/cfs/cdirs/m3992/png/"
+        if propfile is None:
+            propfile = "/global/cfs/cdirs/m3992/png/num_reso_mos_B_icy1_icy2_cell_SGnum_pdbid_stolid.txt"
+        if dev is None:
+            dev = "cuda:0"
+
+        self.fnames = glob.glob(os.path.join(pngdir, "*%s.png" % quad))
+        assert self.fnames
+        self.nums = [self.get_num(f) for f in self.fnames]
+        self.img_sh = 546, 518
+        self.props = ["reso"]
+
+        self.prop = pandas.read_csv(
+            propfile,
+            delimiter=r"\s+",
+            names=["num", "reso", "mos", "B", "icy1", "icy2", "cell1", \
+                   "cell2", "cell3", "SGnum", "pdbid", "stolid"])
+
+        self.labels = self.prop[["num", "reso"]]
+        if invert_res:
+            self.labels.reso = 1/self.labels.reso
+
+        self.dev = dev  # pytorch device ID
+
+        Ntotal = len(self.fnames)
+        if start is None:
+            start = 0
+        if stop is None:
+            stop = Ntotal
+        assert start >= 0
+        assert stop <= Ntotal
+        assert stop > start
+        self.start = start
+        self.stop = stop
+
+    @staticmethod
+    def get_num(f):
+        s = re.search("sim_[0-9]{5}", f)
+        num = f[s.start(): s.end()].split("sim_")[1]
+        return int(num)
+
+    @property
+    def dev(self):
+        return self._dev
+
+    @dev.setter
+    def dev(self, val):
+        self._dev = val
+
+    def __len__(self):
+        return self.stop - self.start
+
+    def __getitem__(self, i):
+        assert self.dev is not None, "Set the dev (torch device) property first!"
+
+        img = Image.open(self.fnames[i])
+        img_dat = np.reshape(img.getdata(), self.img_sh).astype(np.float32)
+
+        num = self.nums[i]
+        img_lab = self.labels.query("num==%d" % num)
+
+        img_dat = torch.tensor(img_dat[None]).to(self.dev)
+        img_lab = torch.tensor(img_lab.values).to(self.dev)
+        return img_dat, img_lab
+
+
+if __name__=="__main__":
+
+    train_imgs = TorchDset(start=2000, stop=9000)
+    train_imgs_validate = TorchDset(start=2000, stop=3000)
+    test_imgs = TorchDset(start=1000, stop=2000)
+
+    train_tens = DataLoader(train_imgs, batch_size=16, shuffle=True)
+    train_tens_validate = DataLoader(train_imgs_validate, batch_size=16, shuffle=True)
+    test_tens = DataLoader(test_imgs, batch_size=16, shuffle=True)
+
+    from IPython import embed;embed()
