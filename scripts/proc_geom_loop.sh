9c0c42a4fce87d857bd50d80905a029438cdc69c
dermen <dermen@lbl.gov>
2023-05-06 09:21:27 -0700

    proc_geom_joblib version

--- scripts/proc_geom_loop.sh
+++ scripts/proc_geom_loop.sh
@@ -3,9 +3,10 @@
 moredata=$1
 model=$2
 nproc=$3
+arch=$4
 
 count=1
-for dirname in `find ${moredata} -maxdepth 1 -type d -name "[0-9]*[0-9]A"`
+for dirname in `find ${moredata} -maxdepth 1 -type d,l -name "[0-9]*[0-9]A"`
 do
     # resolution target
     res=$(basename $dirname)
@@ -14,7 +15,8 @@ do
     outname=results.${count}.${res}
 
     # evaulate the model
-    mpirun -n $nproc python $MODZ/resonet/examples/proc_geom.py  $dirname $model ${outname} --predictor one_over_res --geom 
+    libtbx.python $MODZ/resonet/examples/proc_geom_joblib.py  $dirname $model ${outname} --predictor one_over_res \
+    --geom --njobs $nproc --sanitize --arch $arch
 
     count=$[$count +1]
 done
